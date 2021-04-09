518415a385037b895ee0ec3351a5f3901c48e9d3
Dmitry <d.kochetov@initlab.ru>
2021-04-09 17:59:30 +0300

    Create d8c_actualization.sh

---
+++ d8c_actualization.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER="$1"
+HOST="$2"
+PORT="$3"
+#dev
+PATH_SOURCE="$4"
+#prod
+PATH_DEST="$5"
+
+echo "Copy sites/default/files"
+rsync -av -e "ssh -p $PORT -i ~/.ssh/id_rsa_ci" "$USER"@"$HOST":"$PATH_DEST/web/sites/default/files/" "$PATH_SOURCE/web/sites/default/files/" --exclude 'settings.php'
+echo "Create dump to $PATH_SOURCE/mysql/db.sql"
+mkdir -p $PATH_SOURCE/mysql
+ssh -i ~/.ssh/id_rsa_ci -p $PORT "$USER"@"$HOST" /usr/local/bin/drush --root=$PATH_DEST sql-dump > $PATH_SOURCE/mysql/db.sql
+echo "Restore from dump $PATH_SOURCE/mysql/db.sql"
+drush --root=$PATH_SOURCE sql-drop -y 
+drush --root=$PATH_SOURCE sql-cli < $PATH_SOURCE/mysql/db.sql
+echo "Pull code"
+cd $PATH_SOURCE
+git reset --hard
+git checkout master
+git pull origin master
+echo "Install site"
+composer install
+drush updb -y
+drush cr
