7c7764edb8afe296346102724c38a891f5862025
Dmitry <d.kochetov@initlab.ru>
2021-06-15 16:51:56 +0300

    Update d7c_actualization.sh

--- d7c_actualization.sh
+++ d7c_actualization.sh
@@ -9,10 +9,10 @@ PATH_SOURCE="$4"
 PATH_DEST="$5"
 
 echo "Copy sites/default/files"
-rsync -av -e "ssh -p $PORT -i $HOME/.ssh/id_rsa_ci" "$USER"@"$HOST":"$PATH_DEST/sites/default/files/" "$PATH_SOURCE/sites/default/files/" --exclude 'settings.php'
+rsync -av -e "ssh -p $PORT -i ~/.ssh/id_rsa_ci" "$USER"@"$HOST":"$PATH_DEST/sites/default/files/" "$PATH_SOURCE/sites/default/files/" --exclude 'settings.php'
 echo "Create dump to $PATH_SOURCE/mysql/db.sql"
 mkdir -p $PATH_SOURCE/mysql
-ssh -i $HOME/.ssh/id_rsa_ci -p $PORT "$USER"@"$HOST" drush --root=$PATH_DEST sql-dump > $PATH_SOURCE/mysql/db.sql
+ssh -i ~/.ssh/id_rsa_ci -p $PORT "$USER"@"$HOST" drush --root=$PATH_DEST sql-dump > $PATH_SOURCE/mysql/db.sql
 echo "Restore from dump $PATH_SOURCE/mysql/db.sql"
 drush --root=$PATH_SOURCE sql-drop -y
 drush --root=$PATH_SOURCE sql-cli < $PATH_SOURCE/mysql/db.sql
