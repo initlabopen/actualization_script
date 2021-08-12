88cda4e76af4d4c3b7c415144a4722f10265adc6
Dmitry <d.kochetov@initlab.ru>
2021-08-12 14:48:39 +0300

    Create deploy_script_d7c_master_intuit.sh

---
+++ deploy/deploy_script_d7c_master_intuit.sh
@@ -0,0 +1,8 @@
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
+echo "Modify Files = $check_modify_files"
+if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
+ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
+ssh -p $1 $2@$3 "cd $4 && git checkout master"
+ssh -p $1 $2@$3 "cd $4 && git pull origin master"
+ssh -p $1 $2@$3 "cd $4 && drush -u 1 updb -y"
+ssh -p $1 $2@$3 "cd $4 && drush -u 1 cc all"
