a76e7c0ce149a5bf2fe81dacde7beed312869460
Dmitry <d.kochetov@initlab.ru>
2021-08-12 13:45:11 +0300

    Create deploy_script.sh

---
+++ deploy/deploy_script.sh
@@ -0,0 +1,12 @@
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cex -y"
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
+echo "Modify Files = $check_modify_files"
+if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
+ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
+ssh -p $1 $2@$3 "cd $4 && git checkout master"
+ssh -p $1 $2@$3 "cd $4 && git pull origin master"
+ssh -p $1 $2@$3 "cd $4 && composer install"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush updatedb -y"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cache:rebuild"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush config:import -y"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cache:rebuild"
