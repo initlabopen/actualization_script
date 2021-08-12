633f4001ba8ec4cc2d9b11371d8ffc8f5c0da7e8
Dmitry <d.kochetov@initlab.ru>
2021-08-12 14:28:03 +0300

    Create deploy_script_d7c_branch.sh

---
+++ deploy/deploy_script_d7c_branch.sh
@@ -0,0 +1,9 @@
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
+echo "Modify Files = $check_modify_files"
+if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
+
+ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
+ssh -p $1 $2@$3 "cd $4 && git checkout test"
+ssh -p $1 $2@$3 "cd $4 && git pull origin ${CI_COMMIT_BRANCH}"
+ssh -p $1 $2@$3 "cd $4 && drush updb -y"
+ssh -p $1 $2@$3 "cd $4 && drush cc all"
