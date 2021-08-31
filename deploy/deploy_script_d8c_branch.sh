91542eeddfdddae4428aaa0e72f4c2c3a319830c
Dmitry <d.kochetov@initlab.ru>
2021-08-31 16:17:16 +0300

    Create deploy_script_d8c_branch.sh

---
+++ deploy/deploy_script_d8c_branch.sh
@@ -0,0 +1,14 @@
+set -e
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cex -y"
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
+echo "Modify Files = $check_modify_files"
+if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
+
+ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
+ssh -p $1 $2@$3 "cd $4 && git checkout test"
+ssh -p $1 $2@$3 "cd $4 && git pull origin ${CI_COMMIT_BRANCH}"
+ssh -p $1 $2@$3 "cd $4 && composer install"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush updatedb -y"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cache:rebuild"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush config:import -y"
+ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cache:rebuild"
