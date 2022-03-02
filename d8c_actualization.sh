408cd45f5581db4728fbda4384380eaaaef590d8
Dmitry <d.kochetov@initlab.ru>
2022-03-02 12:54:04 +0300

    Update d8c_actualization.sh

--- d8c_actualization.sh
+++ d8c_actualization.sh
@@ -12,7 +12,7 @@ echo "Copy sites/default/files"
 rsync -av -e "ssh -p $PORT -i ~/.ssh/id_rsa_ci" "$USER"@"$HOST":"$PATH_DEST/web/sites/default/files/" "$PATH_SOURCE/web/sites/default/files/" --exclude 'settings.php'
 echo "Create dump to $PATH_SOURCE/mysql/db.sql"
 mkdir -p $PATH_SOURCE/mysql
-ssh -i ~/.ssh/id_rsa_ci -p $PORT "$USER"@"$HOST" /usr/local/bin/drush --root=$PATH_DEST sql-dump > $PATH_SOURCE/mysql/db.sql
+ssh -i ~/.ssh/id_rsa_ci -p $PORT "$USER"@"$HOST" drush --root=$PATH_DEST sql-dump > $PATH_SOURCE/mysql/db.sql
 echo "Restore from dump $PATH_SOURCE/mysql/db.sql"
 drush --root=$PATH_SOURCE sql-drop -y 
 drush --root=$PATH_SOURCE sql-cli < $PATH_SOURCE/mysql/db.sql
