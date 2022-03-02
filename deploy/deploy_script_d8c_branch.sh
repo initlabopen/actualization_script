837eee8c3ca019913882bdff89a3789694572061
Dmitry <d.kochetov@initlab.ru>
2022-03-02 12:55:22 +0300

    Update deploy_script_d8c_branch.sh

--- deploy/deploy_script_d8c_branch.sh
+++ deploy/deploy_script_d8c_branch.sh
@@ -1,5 +1,5 @@
 set -e
-ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cex -y"
+ssh -p $1 $2@$3 "cd $4 && drush cex -y"
 check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
 echo "Modify Files = $check_modify_files"
 if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
@@ -8,7 +8,7 @@ ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
 ssh -p $1 $2@$3 "cd $4 && git checkout test"
 ssh -p $1 $2@$3 "cd $4 && git pull origin ${CI_COMMIT_BRANCH}"
 ssh -p $1 $2@$3 "cd $4 && composer install"
-ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush updatedb -y"
-ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cache:rebuild"
-ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush config:import -y"
-ssh -p $1 $2@$3 "cd $4 && /usr/local/bin/drush cache:rebuild"
+ssh -p $1 $2@$3 "cd $4 && drush updatedb -y"
+ssh -p $1 $2@$3 "cd $4 && drush cache:rebuild"
+ssh -p $1 $2@$3 "cd $4 && drush config:import -y"
+ssh -p $1 $2@$3 "cd $4 && drush cache:rebuild"
