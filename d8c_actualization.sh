ece59278802607948dc807040eb3d810cc84aeb1
k.kulkov <k.kulkov@initlab.ru>
2022-06-01 13:06:05 +0300

    Recreate test branch on actualization dev

--- d8c_actualization.sh
+++ d8c_actualization.sh
@@ -14,14 +14,30 @@ rsync -av -e "ssh -p $PORT -i ~/.ssh/id_rsa_ci" "$USER"@"$HOST":"$PATH_DEST/web/
 echo "Create dump to $PATH_SOURCE/mysql/db.sql"
 mkdir -p $PATH_SOURCE/mysql
 ssh -i ~/.ssh/id_rsa_ci -p $PORT "$USER"@"$HOST" drush --root=$PATH_DEST sql-dump > $PATH_SOURCE/mysql/db.sql
+echo "Save configs if exists"
+cd $PATH_SOURCE
+git reset --hard
+git checkout test
+drush --root=$PATH_SOURCE cex -y
+check_modify_files=$(git diff -q)
+if [ -n "$check_modify_files" ]; then
+  old_branch=$(git branch | grep "test-old")
+  if [ "$old_branch" ]; then
+  	git branch -D test-old
+  fi
+  git add config/sync
+  git commit -m "add configs"
+  git checkout -b test-old
+fi
 echo "Restore from dump $PATH_SOURCE/mysql/db.sql"
 drush --root=$PATH_SOURCE sql-drop -y 
 drush --root=$PATH_SOURCE sql-cli < $PATH_SOURCE/mysql/db.sql
 echo "Pull code"
-cd $PATH_SOURCE
-git reset --hard
 git checkout master
 git pull origin master
+git branch -D test
+git checkout -b test
+git push origin test --force
 echo "Install site"
 composer install
 drush updb -y
