f4ba847cf3e2cad6520f75efbbf0872600aa2138
Dmitry <d.kochetov@initlab.ru>
2022-07-26 09:13:45 +0300

    Update deploy_script_d8c_neo.sh

--- deploy/deploy_script_d8c_neo.sh
+++ deploy/deploy_script_d8c_neo.sh
@@ -1,6 +1,6 @@
 set -e
 ssh -p $1 $2@$3 "cd $4/docroot && drush cex -y"
-check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff --name-only")
 echo "Modify Files = $check_modify_files"
 if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
 ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
