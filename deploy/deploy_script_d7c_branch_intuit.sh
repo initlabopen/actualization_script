732acc469f80fa8f32bd8ddfb12a3fb1e09840cf
Dmitry <d.kochetov@initlab.ru>
2022-07-26 09:13:07 +0300

    Update deploy_script_d7c_branch_intuit.sh

--- deploy/deploy_script_d7c_branch_intuit.sh
+++ deploy/deploy_script_d7c_branch_intuit.sh
@@ -1,5 +1,5 @@
 set -e
-check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff -q")
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff --name-only")
 echo "Modify Files = $check_modify_files"
 if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
 
