6bd1f0e39f9a90733d064a8bb738c54f4de5428a
ratkoilya <103240826+ratkoilya@users.noreply.github.com>
2022-10-12 16:59:52 +0500

    Create deploy_script_d9c_astradetail.sh

---
+++ deploy/deploy_script_d9c_astradetail.sh
@@ -0,0 +1,13 @@
+set -e
+ssh -p $1 $2@$3 "cd $4 && drush cex -y"
+check_modify_files=$(ssh -p $1 $2@$3 "cd $4 && git diff --name-only")
+echo "Modify Files = $check_modify_files"
+if [ -n "$check_modify_files" ]; then echo "FIND MODIFY FILES"; exit 1; fi
+ssh -p $1 $2@$3 "cd $4 && git reset --hard HEAD"
+ssh -p $1 $2@$3 "cd $4 && git checkout origin/master"
+ssh -p $1 $2@$3 "cd $4 && git pull origin master"
+ssh -p $1 $2@$3 "cd $4 && composer install"
+ssh -p $1 $2@$3 "cd $4 && drush updatedb -y"
+ssh -p $1 $2@$3 "cd $4 && drush cache:rebuild"
+ssh -p $1 $2@$3 "cd $4 && drush config:import -y"
+ssh -p $1 $2@$3 "cd $4 && drush cache:rebuild"
